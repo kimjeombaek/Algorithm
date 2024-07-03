978b88360765c47fcb8bbd277aa7cf3ac2ba612a
KIm YongHyeon <106867454+kimjeombaek@users.noreply.github.com>
2024-07-03 20:02:41 +0900

    [Silver IV] Title: 카드2, Time: 292 ms, Memory: 54852 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+n = int(input())
+lst = deque([i for i in range(1, n+1)])
+
+while (len(lst) > 1):
+    lst.popleft()
+    back = lst.popleft()
+    lst.append(back)
+print(lst[0])
